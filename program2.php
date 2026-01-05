ef093c6807edbe5e7383ba02f68f26f56becd37f
senthamilselvi21 <senthamilsevie@gmail.com>
2026-01-05 09:35:04 +0530

    Add PHP script to display salaries in a table

---
+++ program2.php
@@ -0,0 +1,12 @@
+<?php 
+$a=1000; 
+$b=1200;
+$c=1400;
+ 
+echo "<table border=5 cell spacing=1 cellpadding=15> 
+<tr> <td><font color=green>Salary of Mr. A is</td> <td>$a$</font></td></tr>  
+<tr> <td><font color=blue>Salary of Mr. B is</td> <td>$b$</font></td></tr> 
+<tr> <td><font color=yellow>Salary of Mr. C is</td> 
+<td>$c$</font></td></tr> 
+</table>"; 
+?> 
